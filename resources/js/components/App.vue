90a2ba75723a85b63aa8fe75d135d505fd79f564
Angel Mendoza <1930.kick@gmail.com>
2019-11-30 00:27:39 -0500

    add: styles to Nav.vue component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
+  <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+    <Nav />
     <router-view />
   </div>
 </template>
 
 <script>
+import Nav from './Nav'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Nav
+  }
 }
 </script>
