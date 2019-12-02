22d9643a5f01191d7e78955e4d84df8789bc86f0
Angel Mendoza <1930.kick@gmail.com>
2019-12-01 22:24:57 -0500

    add: styles to Sidebar.vue component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
     <Nav />
-    <router-view />
+    <div class="flex overflow-y-hidden flex-1">
+      <Sidebar />
+
+      <div class="overflow-x-hidden w-2/3">
+        <router-view />
+        <p v-for="i in 100" :key="i">Gaaaa</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
 import Nav from './Nav'
+import Sidebar from './Sidebar'
+
 export default {
   name: 'App',
   components: {
-    Nav
+    Nav,
+    Sidebar
   }
 }
 </script>
