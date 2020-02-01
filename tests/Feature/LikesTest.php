521c61247488fccfd75903c2c6a69ac0501848c2
Alfredo Mendoza <1930.kick@gmail.com>
2020-02-01 00:15:41 -0500

    Posts Must Return With Likes Test

--- tests/Feature/LikesTest.php
+++ tests/Feature/LikesTest.php
@@ -44,4 +44,48 @@ class LikesTest extends TestCase
             ]
         ]);
     }
+
+    /** @test */
+    public function post_are_returned_with_likes()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs($user = factory(User::class)->create(), 'api');
+
+        $post = factory(Post::class)->create([
+            'id' => 123,
+            'user_id' => $user->id
+        ]);
+
+        $this->post("/api/posts/{$post->id}/like")
+            ->assertStatus(Response::HTTP_OK);
+
+        $response = $this->get('/api/posts')
+            ->assertStatus(Response::HTTP_OK);
+
+        $response->assertJson([
+            'data' => [
+                [
+                    'data' => [
+                        'type' => 'posts',
+                        'attributes' => [
+                            'likes' => [
+                                'data' => [
+                                    [
+                                        'data' => [
+                                            'type' => 'likes',
+                                            'like_id' => 1,
+                                            'attributes' => []
+                                        ]
+                                    ]
+                                ],
+                                'like_count' => 1,
+                                'user_likes_post' => true
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
 }
